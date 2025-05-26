96a14348dddb329b6e6bf640edb1a7a2148a77bd
Rharald Moreira Guimaraes <rharaldmoreirag@gmail.com>
2025-05-26 17:16:13 -0400

    Removendo criação automática de tabelas do wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,6 @@
 from app import create_app
-from config import Config
 
 application = create_app()
-
-# Garantir que o banco de dados seja criado
-with application.app_context():
-    from models import db
-    db.create_all()
 
 if __name__ == '__main__':
     application.run()
