bab9dfecb0da70b4321353814063b7c3534eb29d
Rharald Moreira Guimaraes <rharaldmoreirag@gmail.com>
2025-05-26 17:12:21 -0400

    Atualizando dependências e configurando wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,6 @@
-import os
-import logging
 from app import create_app
 from config import Config
-from io import BytesIO
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-import pandas as pd
 
-# Configurar logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler(Config.LOG_FILE),
-        logging.StreamHandler()
-    ]
-)
-
-# Criar e inicializar a aplicação
 application = create_app()
 
 # Garantir que o banco de dados seja criado
