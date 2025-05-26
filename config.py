ed9303589b536f7de50b1a764d0adb02d869225e
Rharald Moreira Guimaraes <rharaldmoreirag@gmail.com>
2025-05-26 17:13:06 -0400

    Atualizando configuração do banco de dados para usar SQLite em memória

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
     
     # Configurações do banco de dados
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///tasks.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///:memory:'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Configurações do e-mail
